c429d08b517da86c69ea33aec65f20a43d62c679
usmanghani043 <this.usmanghani@gmail.com>
2024-09-05 07:15:52 +0000

    Container config done

---
+++ 01_Lecture/variables.js
@@ -0,0 +1,20 @@
+const userId = 43;
+let userName = "Usman Ghani";
+var userAccount = "Ghani_Bhaii"
+
+
+console.table([userId,userName,userAccount]);
+
+/*
+userId=56;
+userName="Huzaifa";
+userAccount="this.mehuzaifa";    //This shows type Error because we want to assign some changes to Constants
+
+console.table([userId,userName,userAccount]);
+*/
+
+
+userName="Huzaifa";
+userAccount="this.mehuzaifa";
+
+console.table([userId,userName,userAccount]);
